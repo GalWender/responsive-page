4cf726a5222c5671963ac5bccde60bbfeb16cf38
Gal Wender <68247697+gwgw6600@users.noreply.github.com>
2022-08-07 23:55:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+
+const elNav = document.querySelector('.main-nav-list')
+// let mqMin = window.matchMedia("(min-width: 640px)")
+// window.onresize = function () {
+//     if (mqMin.matches) {
+//         console.log('ok')
+//         elNav.classList.toggle('open')
+//         console.log(elNav)
+//     }
+//   }
+
+function navOpen() {
+        elNav.classList.toggle('open')
+
+}
